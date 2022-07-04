84b8a3e45329318ee03300939e33f4d1a0a5ac80
yagomoraes <yago.moraes@calindra.com.br>
2022-07-04 14:51:33 -0300

    feat: criação (não implementado) o modelo para enriquecer

--- controller/swapiController.js
+++ controller/swapiController.js
@@ -11,48 +11,33 @@ const swapiController = (app) =>{
         const enrichParams = req.query.enrichParams;
         const urlComplete = `${url}/${entity}/${id}`;
 
-    
         if (!enrichParams) {
             const simple = new swapiSimple()
             try {
-                /* const response = await axios.get(urlComplete);
-                const responseData = response.data */
-
                 const responseSimple = await simple.getResponse(urlComplete)
                 res.json(responseSimple);
-                
             } catch (error) {
                 res.json(error)
             }
             
         } else {
-            //const enrich = new swapiEnrich()
             try {
-                //Maneira mais enchuta pra utilizar:
-                /* const responseEnrich = await enrich.getResponse(urlComplete)
-                res.json(responseEnrich) */
-
                 const splitedParams = enrichParams.split(",");
-                //console.log(splitedParams);
     
                 const responseEnrichParams = await axios.get(urlComplete);
                 const responseEnrichParamsData = responseEnrichParams.data
-    
-                
+          
                 const responseEnrichParamsObjKeys = Object.keys(responseEnrichParamsData)
-                //console.log(responseEnrichParamsObjKeys);
     
                 const intersection = splitedParams.filter(element=>responseEnrichParamsObjKeys.includes(element))
-                //console.log(intersection);
     
                 responseEnrichParamsData.enrichParams = intersection
     
                 const newResponseData = responseEnrichParamsData
                 await enrichFunction(splitedParams, newResponseData);
     
-    
-                //res.json(responseEnrichParamsData);
                 res.json(newResponseData)
+
             } catch (error) {
                 res.json(error);
             }
@@ -61,19 +46,3 @@ const swapiController = (app) =>{
 }
 
 export default swapiController
-
-/* async function enrichFunction(splitedParams, newResponseData) {
-    for (let i = 0; i < splitedParams.length; i++) {
-        if (splitedParams[i] in newResponseData) {
-            const detailEntry = newResponseData[splitedParams[i]];
-            //console.log(detailEntry);
-            for (let j = 0; j < detailEntry.length; j++) {
-                const specificEntry = detailEntry[j];
-                const accessSpecificEntry = await axios.get(specificEntry);
-                const accessSpecificEntryData = accessSpecificEntry.data;
-                detailEntry[j] = accessSpecificEntryData;
-            }
-            //console.log(detailEntry) 
-        }
-    }
-} */
