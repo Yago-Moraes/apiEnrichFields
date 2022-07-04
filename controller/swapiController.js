52c5fcbff3f3c96d8e39a37b55fb789d195bef57
yagomoraes <yago.moraes@calindra.com.br>
2022-07-04 17:00:13 -0300

    feat: adição dos timers de entrada e saída

--- controller/swapiController.js
+++ controller/swapiController.js
@@ -7,40 +7,52 @@ const url = "https://swapi.dev/api";
 
 const swapiController = (app) =>{
     app.get("/:entity/:id/", async (req, res) => {
+        const start = new Date().getTime()
+        console.log("Start: "+start);
         const { entity, id } = req.params;
         const enrichParams = req.query.enrichParams;
         const urlComplete = `${url}/${entity}/${id}`;
 
+
+        const simple = new swapiSimple()
         if (!enrichParams) {
-            const simple = new swapiSimple()
             try {
                 const responseSimple = await simple.getResponse(urlComplete)
                 res.json(responseSimple);
             } catch (error) {
                 res.json(error)
+            }finally{
+                const endStart = new Date().getTime()
+                console.log("End: "+endStart);
+                console.log("Diference: "+(endStart-start));
             }
             
         } else {
             try {
                 const splitedParams = enrichParams.split(",");
-    
+
                 const responseEnrichParams = await axios.get(urlComplete);
                 const responseEnrichParamsData = responseEnrichParams.data
           
                 const responseEnrichParamsObjKeys = Object.keys(responseEnrichParamsData)
     
                 const intersection = splitedParams.filter(element=>responseEnrichParamsObjKeys.includes(element))
-    
                 responseEnrichParamsData.enrichParams = intersection
     
-                const newResponseData = responseEnrichParamsData
+                const newResponseData =  JSON.parse(JSON.stringify(responseEnrichParamsData)) 
                 await enrichFunction(splitedParams, newResponseData);
     
                 res.json(newResponseData)
 
             } catch (error) {
                 res.json(error);
+            }finally{
+                const final = new Date().getTime()
+                console.log("Final: "+final);
+                console.log("Diference: "+(final-start));
             }
+            
+        
         }
     });
 }
