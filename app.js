084de6046b8f290cf143e45bf687ff284cad2df0
yagomoraes <yago.moraes@calindra.com.br>
2022-07-04 17:24:42 -0300

    fix: ajuste de local do controller

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import swapiController from "./controller/swapiController.js";
+import swapiController from "./src/controller/swapiController.js";
 
 const app = express();
 
